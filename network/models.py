6be2d283571d5eacc308f170ffc51b4981c09a05
jhelum13 <jngelinas@yahoo.com>
2022-05-12 21:17:26 -0700

    set up models and pagination

--- network/models.py
+++ network/models.py
@@ -4,3 +4,33 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    text = models.TextField(max_length=1000)
+    title = models.CharField(max_length=100)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        "User",
+        on_delete=models.CASCADE,  # if user is deleted, delete likes associated
+        related_name="like",
+    )
+    post = models.ForeignKey(
+        "Post",
+        on_delete=models.CASCADE,  # if post is deleted, delete likes associated
+        related_name="like",
+    )
+
+class Follower:
+    following = models.ForeignKey(
+        "User",
+        on_delete=models.CASCADE,  # if user is deleted, delete follower associated
+        related_name="following",
+    )
+    follower = models.ForeignKey(
+        "User",
+        on_delete=models.CASCADE,  # if user is deleted, delete follower associated
+        related_name="follower",
+    )
--- network/views.py
+++ network/views.py
@@ -1,14 +1,30 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
+from django.core.paginator import Paginator
+from .models import Post
+
+# from requests import request
+from django.contrib.auth.decorators import login_required
 
 from .models import User
 
-
 def index(request):
-    return render(request, "network/index.html")
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            # throw error if post
+            return error(request, "Invalid Submission")
+        else:
+            posts = Post.objects.all()
+
+            paginator = Paginator(posts, 10) # Show 10 contacts per page.
+            page_number = request.GET.get('page')
+            page_obj = paginator.get_page(page_number)
+            return render(request, "network/index.html", {'page_obj': page_obj})    
+    else:
+        return redirect(reverse("login"))
 
 
 def login_view(request):
@@ -61,3 +77,40 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+def newpost(request):
+    if request.method == "POST":
+        # create post
+        pass
+    else:
+
+        return redirect(reverse("index"))
+
+
+
+def profile(request):
+    if request.method == "POST":
+        pass
+    else:
+        pass
+
+
+
+
+def following(request):
+    if request.method == "POST":
+        pass
+    else:
+        pass
+
+
+def editapi(request):
+    if request.method == "POST":
+        pass
+    else:
+        pass
+
+def error(request, errortext):
+    return render(request, "network/index.html", {
+        "errortext": errortext
+    })
